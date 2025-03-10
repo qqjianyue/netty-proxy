c79c81d8a791aebc7985e5a60f5764397cd8f6bd
qqjianyue <lijiany_sysu@yeah.net>
2025-03-10 14:51:48 +0800

    feat(proxy): add netty proxy implementation
    - Implement RouterServer, RouterClientHandler, and RouterServerHandler classes- Add Maven project configuration (pom.xml)
    - Include .gitignore file with common IDE and build tool exclusions

---
+++ src/main/java/org/opensource/proxy/RouterClientHandler.java
@@ -0,0 +1,69 @@
+package org.opensource.proxy;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class RouterClientHandler extends ChannelInboundHandlerAdapter {
+
+    private final String host;
+    private final int port;
+    private Channel outboundChannel;
+
+    public RouterClientHandler(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap b = new Bootstrap();
+        b.group(group)
+         .channel(NioSocketChannel.class)
+         .handler(new ChannelInitializer<SocketChannel>() {
+             @Override
+             public void initChannel(SocketChannel ch) throws Exception {
+                 ch.pipeline().addLast(new RouterServerHandler(ctx.channel()));
+             }
+         });
+
+        ChannelFuture f = b.connect(host, port).sync();
+        outboundChannel = f.channel();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (outboundChannel.isActive()) {
+            outboundChannel.writeAndFlush(msg).addListener((ChannelFutureListener) future -> {
+                if (future.isSuccess()) {
+                    ctx.channel().read();
+                } else {
+                    future.channel().close();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        if (outboundChannel != null) {
+            closeOnFlush(outboundChannel);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        closeOnFlush(ctx.channel());
+    }
+
+    static void closeOnFlush(Channel ch) {
+        if (ch.isActive()) {
+            ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+}
---
+++ src/main/java/org/opensource/proxy/RouterServerHandler.java
@@ -0,0 +1,42 @@
+package org.opensource.proxy;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class RouterServerHandler extends ChannelInboundHandlerAdapter {
+
+    private final Channel inboundChannel;
+
+    public RouterServerHandler(Channel inboundChannel) {
+        this.inboundChannel = inboundChannel;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        inboundChannel.read();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        inboundChannel.writeAndFlush(msg).addListener((ChannelFutureListener) future -> {
+            if (future.isSuccess()) {
+                ctx.channel().read();
+            } else {
+                future.channel().close();
+            }
+        });
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        RouterClientHandler.closeOnFlush(inboundChannel);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        RouterClientHandler.closeOnFlush(ctx.channel());
+    }
+}
